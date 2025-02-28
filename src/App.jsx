cd8eaf4a07131f35def42555dd911ef478e89650
Tommy <tommasogalistu92@gmail.com>
2025-02-28 14:33:34 +0100

    feat: add the calculate of investment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Results from "./components/Results";
 
 function App() {        
   const [userInput, setUserInput] = useState({
-    initialInvestment: 1000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10
@@ -15,7 +15,7 @@ function App() {
     setUserInput(prevUserInput =>{
         return {
             ...prevUserInput,
-            [inputIdentifier]: newValue
+            [inputIdentifier]: +newValue
         }
     })
   }
